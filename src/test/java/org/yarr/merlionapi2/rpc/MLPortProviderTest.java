a0f61d98d3f5d9cb892cac1ab645f33dd6131051
Dmitry V <savraz@gmail.com>
2015-07-18 23:50:58 +0300

    its actually working

--- src/test/java/org/yarr/merlionapi2/rpc/MLPortProviderTest.java
+++ src/test/java/org/yarr/merlionapi2/rpc/MLPortProviderTest.java
@@ -1,6 +1,9 @@
 package org.yarr.merlionapi2.rpc;
 
+import https.api_merlion_com.dl.mlservice3.ArrayOfString;
 import https.api_merlion_com.dl.mlservice3.CatalogResult;
+import https.api_merlion_com.dl.mlservice3.ItemsAvailResult;
+import https.api_merlion_com.dl.mlservice3.ItemsResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -21,5 +24,16 @@ public class MLPortProviderTest extends AbstractTestNGSpringContextTests {
     public void test() {
         List<CatalogResult> resultList = mlPortProvider.get().getCatalog("B10202").getItem();
         Assert.assertTrue(resultList.size() > 0);
+    }
+
+    @Test(enabled = false)
+    public void testRetrieveItems() {
+        ArrayOfString ids = new ArrayOfString();
+        ids.getItem().add("931772");
+        ids.getItem().add("931786");
+        List<ItemsAvailResult>  itemsAvail = mlPortProvider.get().getItemsAvail("", "ДОСТАВКА", "06-05-15", "", ids).getItem();
+        List<ItemsResult> items = mlPortProvider.get().getItems("", ids, "ДОСТАВКА", 0, 10000, "").getItem();
+        Assert.assertTrue(itemsAvail.size() == 2);
+        Assert.assertTrue(items.size() == 2);
     }
 }
